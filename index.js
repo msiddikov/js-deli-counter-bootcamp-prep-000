6d426dbfb1f49a55ef149e8f2a8b8452ad48d82e
Mukhammadayub Siddikov <m.siddikov@gmail.com>
2017-09-13 10:39:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ function takeANumber(line, name){
   for(var i=0; i<line.length;i++){
     if (line[i]==name) {break;}
   }
+  if (i==line.length){line.push(name)}
   return `Welcome, ${name}. You are number ${++i} in line.`;
 }
 
 function nowServing(line){
   var serv = ""
   if (line.length>0){
-    serv = `Currently serving ${line.shift()}`
+    serv = `Currently serving ${line.shift()}.`
   }else{
     serv = "There is nobody waiting to be served!"
   }
@@ -19,7 +20,7 @@ function currentLine(line){
   var str = "The line is currently: "
   for (let i=0; i<line.length; i++){
     if(str != "The line is currently: " ){str+=", "}
-    str+=`${++i}. ${line[i]}`
+    str+=`${i+1}. ${line[i]}`
   }
   if (str=="The line is currently: "){str = "The line is currently empty."}
   return str;
